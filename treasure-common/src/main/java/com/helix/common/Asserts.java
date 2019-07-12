185fddc0f399d613c2f19b19dd3e1e6c1279d4a8
lijianyu <lijianyu@yunloan.net>
2019-07-12 18:31:01 +0800

    add utils

--- treasure-common/src/main/java/com/helix/common/Asserts.java
+++ treasure-common/src/main/java/com/helix/common/Asserts.java
@@ -1,7 +1,7 @@
 package com.helix.common;
 
 /**
- * @author lijianyu
+ * @author ljy
  * @date 2019/3/1 18:55
  */
 public class Asserts {
@@ -10,4 +10,31 @@ public class Asserts {
             throw new NullPointerException("参数不能为空");
         }
     }
+
+    /**
+     * Assert that an object is not {@code null}.
+     * <pre class="code">Assert.notNull(clazz, "The class must not be null");</pre>
+     * @param message the exception message to use if the assertion fails
+     * @throws IllegalArgumentException if the object is {@code null}
+     */
+    public static void notNull(Object object, String message) {
+        if (object == null) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    /**
+     * Assert a boolean expression, throwing an {@code IllegalArgumentException}
+     * if the expression evaluates to {@code false}.
+     * <pre class="code">Assert.isTrue(i &gt; 0, "The value must be greater than zero");</pre>
+     * @param expression a boolean expression
+     * @param message the exception message to use if the assertion fails
+     * @throws IllegalArgumentException if {@code expression} is {@code false}
+     */
+    public static void isTrue(boolean expression, String message) {
+        if (!expression) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
 }
---
+++ treasure-common/src/main/java/com/helix/common/ReflectionUtil.java
@@ -0,0 +1,75 @@
+package com.helix.common;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+/**
+ * @author ljy
+ * @date 2019/7/9 21:13
+ */
+public class ReflectionUtil {
+
+    /**
+     *
+     * @param clazz
+     * @param name
+     * @param paramTypes
+     * @return
+     */
+    public static Method findMethod(Class<?> clazz, String name, Class<?>... paramTypes) {
+        Asserts.notNull(clazz, "Class must not be null");
+        Asserts.notNull(name, "Method name must not be null");
+
+        Class<?> searchType = clazz;
+        while (searchType != null) {
+            Method[] methods = (searchType.isInterface() ? searchType.getMethods() : clazz.getDeclaredMethods());
+            for (Method method : methods) {
+                if (name.equals(method.getName()) &&
+                        (paramTypes == null || Arrays.equals(paramTypes, method.getParameterTypes()))) {
+                    return method;
+                }
+            }
+            searchType = searchType.getSuperclass();
+        }
+        return null;
+    }
+
+    /**
+     * Attempt to find a {@link Field field} on the supplied {@link Class} with the
+     * supplied {@code name}. Searches all superclasses up to {@link Object}.
+     * @param clazz the class to introspect
+     * @param name the name of the field
+     * @return the corresponding Field object, or {@code null} if not found
+     */
+    public static Field findField(Class<?> clazz, String name) {
+        return findField(clazz, name, null);
+    }
+
+    /**
+     * Attempt to find a {@link Field field} on the supplied {@link Class} with the
+     * supplied {@code name} and/or {@link Class type}. Searches all superclasses
+     * up to {@link Object}.
+     * @param clazz the class to introspect
+     * @param name the name of the field (may be {@code null} if type is specified)
+     * @param type the type of the field (may be {@code null} if name is specified)
+     * @return the corresponding Field object, or {@code null} if not found
+     */
+    public static Field findField(Class<?> clazz,String name,Class<?> type) {
+        Asserts.notNull(clazz, "Class must not be null");
+        Asserts.isTrue(name != null || type != null, "Either name or type of the field must be specified");
+        Class<?> searchType = clazz;
+        while (Object.class != searchType && searchType != null) {
+            Field[] fields = clazz.getDeclaredFields();
+            for (Field field : fields) {
+                if ((name == null || name.equals(field.getName())) &&
+                        (type == null || type.equals(field.getType()))) {
+                    return field;
+                }
+            }
+            searchType = searchType.getSuperclass();
+        }
+        return null;
+    }
+
+}
