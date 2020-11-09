62cb9f9640cb0769b25959edd4bcd3948f4bbcd5
zny818@163.com <zny818@163.com>
2020-11-09 18:14:20 +0800

    jmh demo没跑成功，待续

---
+++ treasure-demo/src/test/java/com/helix/demo/test/benchmark/BenchmarkTest.java
@@ -0,0 +1,38 @@
+package com.helix.demo.test.benchmark;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 没有测试成功后续在实验
+ */
+
+@BenchmarkMode(Mode.Throughput)
+//@OutputTimeUnit(TimeUnit.MICROSECONDS) // 输出结果的时间粒度为微秒
+@State(Scope.Benchmark)
+public class BenchmarkTest {
+
+
+    @Benchmark
+    public void sleep() throws Exception{
+        System.out.println("sleep");
+//        Thread.sleep(2000L);
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        System.out.println("aaa3");
+        Options options = new OptionsBuilder()
+                .include(BenchmarkTest.class.getName()+".*")
+//                .output("E:/Benchmark.log")
+                .forks(2)
+                .warmupIterations(5)
+                .measurementIterations(2)
+                .build();
+        new Runner(options).run();
+    }
+}
