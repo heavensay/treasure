3f583416813621b01bf876fdc86f080f6c063161
lijianyu <zny818@163.com>
2021-04-24 19:33:07 +0800

    map struct

---
+++ treasure-common/src/test/java/com/helix/common/bean/mapstruct/MapStructUtilTest.java
@@ -0,0 +1,45 @@
+package com.helix.common.bean.mapstruct;
+
+import com.helix.common.bean.dozer.DozerBeanMapperUtil;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author lijianyu
+ * @date 2018/8/11 16:57
+ */
+public class MapStructUtilTest {
+
+    Bean1 bean1;
+
+    @Before
+    public void initBean(){
+        bean1 = new Bean1();
+        bean1.setId(1);
+        bean1.setContent("good");
+        bean1.setMoney(new BigDecimal(5.3));
+        bean1.setUserName("tom");
+        Inner1 inner1 = new Inner1();
+        inner1.setId(2);
+        inner1.setInenerName("inner bean");
+        bean1.setInner1(inner1);
+
+        List<Inner1> list = new ArrayList<Inner1>();
+        list.add(inner1);
+        bean1.setInner1s(list);
+    }
+
+    @Test
+    public void map() {
+        Bean2 bean2 = MapStructUtil.INSTANCE.toBean2(bean1);
+
+        Assert.assertNotNull(bean2.getInner1());
+        Assert.assertNotNull(bean2.getMoney());
+        Assert.assertNotNull(bean2.getUser_name());
+    }
+}
